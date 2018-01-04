da21dbac94c2c8ff6a3462fcf1bde78bacb4d701
Luong Vo <vodailuong@gmail.com>
2018-01-05 00:46:51 +0700

    add test

---
+++ app/src/androidTest/java/vn/luongvo/circleci/sample/MainActivityTest.java
@@ -0,0 +1,31 @@
+package vn.luongvo.circleci.sample;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testHello() throws Exception {
+        onView(withText("Hello World!")).check(matches(isDisplayed()));
+    }
+}
